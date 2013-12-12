1dd0cc561114e62afe86e66f5024c3af03c2dba9
Gabriel Garcia <gabriel.garcia@citrusbyte.com>
2013-12-12 14:51:50 -0200

    Added build file with start/end fragments and almond as a substitute to requirejs so we can make the library stand-alone.

---
+++ src/build.js
@@ -0,0 +1,10 @@
+({
+    baseUrl: ".",
+    name: "lib/almond",
+    include: "m2x",
+    out: "../m2x.min.js",
+    wrap: {
+        startFile: "lib/start.frag",
+        endFile: "lib/end.frag"
+    }
+})
